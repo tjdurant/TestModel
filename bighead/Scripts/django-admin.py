feab99670225d9356caceb40b00e3623c5738b53
Tommy Durant <thomas.durant@yale.edu>
2016-12-04 12:32:35 -0500

    initial commit

---
+++ bighead/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\thoma\Documents\00GitHub\TestModel\TestModel\bighead\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
